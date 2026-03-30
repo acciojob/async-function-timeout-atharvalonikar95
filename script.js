6136e3292daac667d05cbbd338d60cfb20e37307
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-31 02:00:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const msgVal=document.getElementById("text")
 const delayVal=document.getElementById("delay")
 const btn=document.getElementById("btn")
-const opmsg=document.createElement("p")
 const output =document.getElementById("output")
+const opmsg=document.createElement("p")
+output.append(opmsg)
 
 function wait(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 const handleOnSubmit=async()=>{
-	let msg = await msgVal.value;
-	let delay= await delayVal.value;
+	let msg =  msgVal.value;
+	let delay=  delayVal.value;
 	await wait(delay)
 	opmsg.textContent=  msg
 }
 
 btn.addEventListener('click',()=>handleOnSubmit())
-output.append(opmsg)
