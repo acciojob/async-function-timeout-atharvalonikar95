01a007acebee569150e74e0f33bb877bda496f30
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-31 01:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const msgVal=document.getElementById("text")
+const delayVal=document.getElementById("delay")
+const btn=document.getElementById("btn")
+const opmsg=document.getElementById("opmsg")
+
+const handleOnSubmit=async()=>{
+	let msg = await msgVal.value;
+	let delay= await delayVal.value;
+	setTimeout(()=>{
+		opmsg.textContent=  msg
+	}, delay)
+}
+btn.addEventListener('click',()=>handleOnSubmit())
